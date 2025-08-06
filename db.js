21905f13b03ab1249d5234dbec1fe87cb90da0b2
balarcrens <balarcrens@gmail.com>
2025-08-06 17:23:47 +0530

    solve connection uri error

--- db.js
+++ db.js
@@ -7,7 +7,9 @@ dotenv.config();
 const mongodb = async () => {
     const mongoURI = process.env.DB_URI;
 
-    mongoose.connect(mongoURI)
+    mongoose.connect(mongoURI, {
+        dbName: "MovieDb",
+    })
         .then(() => console.log("✅ MongoDB connected"))
         .catch((err) => console.error("❌ MongoDB connection error:", err));
 }
