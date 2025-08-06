fe88745a9c7105b981f63e562b406176fe480287
balarcrens <balarcrens@gmail.com>
2025-08-06 16:33:19 +0530

    change db connection

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const mongodb = async () => {
-    const mongoURI = process.env.MONGODB_URI;
+    const mongoURI = process.env.DB_URI;
 
     mongoose.connect(mongoURI, {
         useNewUrlParser: true,
