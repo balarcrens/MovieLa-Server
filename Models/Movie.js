2f60f8f225c18dd4b909a9eb400a756cebc85e92
balarcrens <balarcrens@gmail.com>
2025-08-26 17:02:57 +0530

    change movie dbmodel

--- Models/Movie.js
+++ Models/Movie.js
@@ -3,41 +3,41 @@ const mongoose = require('mongoose');
 const MovieSchema = new mongoose.Schema({
     movie_name: {
         type: String,
-        required: true,
-        trim: true
+        required: true, trim: true
     },
-    fileid: {
-        type: String,
-        required: true
-    },
-    description: String,
+    fileid: { type: String, required: true },
+    description: { type: String, trim: true },
+    summary: { type: String, trim: true },
+    review: { type: String, trim: true },
     rating: {
         type: Number,
-        min: 0,
-        max: 10
+        min: 0, max: 10
     },
     slug: {
         type: String,
-        required: true,
-        unique: true
+        required: true, unique: true
     },
-    download_link: String,
+    download_link: { type: String, trim: true },
     posterUrl: {
         type: String,
         required: true,
         trim: true
     },
-    trailer_link: String,
-    summary: String,
-    duration: String,
-    size: String,
-    categories: {
-        type: [String],
+    trailer_link: { type: String, trim: true },
+    duration: { type: String, trim: true },
+    size: { type: String, trim: true },
+    releaseDate: { type: String, default: "N/A", trim: true },
+    categories: [{ type: String, trim: true }],
+    screenshots: [{ type: String, trim: true }],
+    actors: [{ type: String, trim: true }],
+    director: { type: String, trim: true },
+    language: {
+        type: String,
+        default: "Hindi",
         trim: true
     },
-    screenshots: {
-        type: [String],
-    }
+    keywords: [{ type: String, trim: true }],
+    meta_description: { type: String, trim: true }
 }, {
     timestamps: true
 });
